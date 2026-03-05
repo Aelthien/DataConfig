cb53a74fda63e4fdb5454bc4ebed2575a582c147
Aelthien <clh@windowslive.com>
2026-03-05 22:32:39 +1300

    Update DcJsonWriter.cpp

--- DataConfig/Source/DataConfigCore/Private/DataConfig/Json/DcJsonWriter.cpp
+++ DataConfig/Source/DataConfigCore/Private/DataConfig/Json/DcJsonWriter.cpp
@@ -222,8 +222,7 @@ static void WriteEscapedString(TSelf* Self, const FString& Str)
 			{
 				if (StringSourceUtils::IsControl(Ch))
 				{
-					const static TCHAR _CONTROL_ESCAPE_FMT[] = { '\\', 'u', '%', '0', '4', 'x', 0 };
-					Escaped.Appendf(_CONTROL_ESCAPE_FMT, Ch);
+					Escaped += FString::Printf(TEXT("\\u%04x"), static_cast<uint32>(Ch));
 				}
 				else
 				{
